bfa89855fb0f36f41ae9de8e1c550b481d176dd6
Claire Cassidy <clacassidy@gmail.com>
2019-05-28 17:24:46 -0700

    created airbenderList.js

---
+++ src/components/AirbenderList.js
@@ -0,0 +1,12 @@
+import Component from './Component.js';
+
+class AirbenderList extends Component {
+
+    renderTemplate() {
+        return /*html*/`
+            <ul class="airbender-list"></ul>
+        `;
+    }
+}
+
+export default AirbenderList;
